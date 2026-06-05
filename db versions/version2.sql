41b083fcaa10c071fe1c9d499501c31cbbfe0339
NelsonCarreraS <nelson.carrera.s@uni.pe>
2026-06-05 11:52:03 -0500

    Modificacion nelson

---
+++ db versions/version2.sql
@@ -0,0 +1,26 @@
+-- AUTOR: NELSON CARRERA
+
+-- DESCRIPCIÓN: MODIIFCACIÓN DE COLUMNAS YA SEA EL TIPO DE DATO O NUEVO CAMPOS EN RESPCTIVAS TABLAS
+
+-- MODIFICACIÓN EN 'registro_acceso'
+ALTER TABLE registro_acceso
+    ADD COLUMN estado_deteccion      VARCHAR(20), 
+    ADD COLUMN latencia_ms           INT,          
+    ADD COLUMN nivel_iluminacion     VARCHAR(20), 
+    ADD COLUMN nivel_obstruccion     VARCHAR(20); 
+
+ALTER TABLE registro_acceso 
+    ALTER COLUMN revisado_por_admin TYPE BOOLEAN 
+    USING (revisado_por_admin::BOOLEAN);
+
+-- MODIFICACIÓN EN 'conductor_ransa'
+ALTER TABLE conductor_ransa 
+  ADD COLUMN empresa_transportista     VARCHAR(150)
+
+-- MODIFICACIÓN EN 'camion_ransa'
+ALTER TABLE camion_ransa          
+  ADD COLUMN observaciones      TEXT
+
+-- MODIFICACIÓN EN 'detalle_pedido_mercancia'
+ALTER TABLE detalle_pedido_mercancia
+  ADD COLUMN tipo_mercancia     VARCHAR(20)
